35c328b5c98a9666e8d92cc19690f23de54bd76d
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-09-15 21:13:10 -0400

    sumAllFib

---
+++ sumOddFibonacci.C
@@ -0,0 +1,42 @@
+// function sumFibs(num) {
+//   //set variables
+//   let m = 0;
+//   let n = 1;
+//   let count = 0;
+
+//   //use a while loop, while n is less than the number passed in
+//   while (n <= num) {
+//     //ES6 destructering
+//     //where first index in left side is = to first index on right
+//     [n, m] = [n + m, n];
+
+//     //check if n is off, if true increase
+//     if (m % 2 !== 0) {
+//       count += m;
+//     }
+//   }
+
+//   return count;
+// }
+
+// sumFibs(4);
+
+#include <stdio.h>
+
+int main()
+{
+  int m = 0;
+  int n = 1;
+  int count = 0;
+
+  while(n<=num)
+  {
+
+  }
+
+
+
+
+
+
+}
